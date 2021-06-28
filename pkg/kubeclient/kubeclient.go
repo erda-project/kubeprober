b21b7f959036a1fb0bfa2a453485a136ac965160
WeiXuSeu <xuweiroot@163.com>
2021-06-28 18:52:51 +0800

    add dns resolution probe check

---
+++ pkg/kubeclient/kubeclient.go
@@ -0,0 +1,21 @@
+package kubeclient
+
+import (
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// Create returns a kubernetes api client
+func Client(kubeConfigFile string) (*kubernetes.Clientset, error) {
+	kubeconfig, err := rest.InClusterConfig()
+	if err != nil {
+		// If not in cluster, use kube config file
+		kubeconfig, err = clientcmd.BuildConfigFromFlags("", kubeConfigFile)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return kubernetes.NewForConfig(kubeconfig)
+}
